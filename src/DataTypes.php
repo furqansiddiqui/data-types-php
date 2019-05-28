a7f9750a579a72c74dff7e8b24c52b9bc95b0fd9
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-05-28 17:31:39 +0500

    version

--- src/DataTypes.php
+++ src/DataTypes.php
@@ -21,9 +21,9 @@ namespace FurqanSiddiqui\DataTypes;
 class DataTypes
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "0.2.22";
+    public const VERSION = "0.2.30";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 222;
+    public const VERSION_ID = 230;
 
     /**
      * Checks if argument is of type String and encoded in Base16
